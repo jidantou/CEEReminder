2ae4f21bcf8844a01273ee0c3c864fbafdafe5a4
jidantou <wang3bo3yang3@126.com>
2022-01-09 08:14:11 +0000

    Update code v2.2, have repaired a few bugs. v2.2, having repaired a few bugs.

---
+++ Code/inc/Process.h
@@ -0,0 +1,16 @@
+// Process.h
+#ifndef _Process_h_
+#define _Process_h_
+
+sbit key0 = P3 ^ 2;  // Enter
+sbit key1 = P3 ^ 3;	 // Memu
+sbit key2 = P3 ^ 6;  // Up
+sbit key3 = P3 ^ 7;  // Down
+
+// Function list
+void KeyPro();
+void Process();
+void InterruptInit();
+void CountPro(uchar N);
+
+#endif
